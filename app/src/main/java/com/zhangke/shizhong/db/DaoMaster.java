4a67d67fdc3ef0918b31528e8632ed9970468d67
ZhangKe <zhangke_711@163.com>
2018-05-09 23:15:09 +0800

    添加计划及短期计划数据库表

--- app/src/main/java/com/zhangke/shizhong/db/DaoMaster.java
+++ app/src/main/java/com/zhangke/shizhong/db/DaoMaster.java
@@ -23,12 +23,14 @@ public class DaoMaster extends AbstractDaoMaster {
     public static void createAllTables(Database db, boolean ifNotExists) {
         ApplicationInfoDao.createTable(db, ifNotExists);
         PlanDao.createTable(db, ifNotExists);
+        ShortPlanDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
         ApplicationInfoDao.dropTable(db, ifExists);
         PlanDao.dropTable(db, ifExists);
+        ShortPlanDao.dropTable(db, ifExists);
     }
 
     /**
@@ -49,6 +51,7 @@ public class DaoMaster extends AbstractDaoMaster {
         super(db, SCHEMA_VERSION);
         registerDaoClass(ApplicationInfoDao.class);
         registerDaoClass(PlanDao.class);
+        registerDaoClass(ShortPlanDao.class);
     }
 
     public DaoSession newSession() {
