cbac3fda1613c390a49fc9d0dc552bf42f752e69
ZhangKe <zhangke_711@163.com>
2018-07-26 00:39:02 +0800

    修改 TODO 界面样式

--- app/src/main/java/com/zhangke/shizhong/page/todo/ShowTodoAdapter.java
+++ app/src/main/java/com/zhangke/shizhong/page/todo/ShowTodoAdapter.java
@@ -2,13 +2,20 @@ package com.zhangke.shizhong.page.todo;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.zhangke.shizhong.R;
+import com.zhangke.shizhong.db.DBManager;
+import com.zhangke.shizhong.db.Todo;
+import com.zhangke.shizhong.db.TodoDao;
+import com.zhangke.shizhong.event.TodoChangedEvent;
 import com.zhangke.shizhong.model.todo.ShowTodoEntity;
 import com.zhangke.shizhong.page.base.BaseRecyclerAdapter;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
@@ -16,12 +23,17 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
+ * 显示待办事项列表
+ * <p>
  * Created by ZhangKe on 2018/7/24.
  */
 public class ShowTodoAdapter extends BaseRecyclerAdapter<BaseRecyclerAdapter.ViewHolder, ShowTodoEntity> {
 
+    private TodoDao mTodoDao;
+
     public ShowTodoAdapter(Context context, List<ShowTodoEntity> listData) {
         super(context, listData);
+        mTodoDao = DBManager.getInstance().getTodoDao();
     }
 
     @NonNull
@@ -34,7 +46,7 @@ public class ShowTodoAdapter extends BaseRecyclerAdapter<BaseRecyclerAdapter.Vie
 
     @Override
     public void onBindViewHolder(@NonNull BaseRecyclerAdapter.ViewHolder holder, int position) {
-        if(holder instanceof TodoHolder){
+        if (holder instanceof TodoHolder) {
             TodoHolder todoHolder = (TodoHolder) holder;
             todoHolder.tvTime.setText(listData.get(position).getTodo().getDate());
             todoHolder.tvTitle.setText(listData.get(position).getTodo().getTitle());
@@ -53,9 +65,31 @@ public class ShowTodoAdapter extends BaseRecyclerAdapter<BaseRecyclerAdapter.Vie
         @BindView(R.id.tv_title)
         TextView tvTitle;
 
+        private AlertDialog todoOptionDialog;
+
         TodoHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            itemView.setOnLongClickListener(v -> {
+                View dialogView = inflater.inflate(R.layout.dialog_todo_option, null);
+                dialogView.findViewById(R.id.tv_complete).setOnClickListener(view -> {
+                    Todo todo = listData.get(getAdapterPosition()).getTodo();
+                    todo.setCompleted(true);
+                    mTodoDao.insertOrReplace(todo);
+                    EventBus.getDefault().post(new TodoChangedEvent());
+                    todoOptionDialog.cancel();
+                });
+                dialogView.findViewById(R.id.tv_delete).setOnClickListener(view -> {
+                    mTodoDao.delete(listData.get(getAdapterPosition()).getTodo());
+                    EventBus.getDefault().post(new TodoChangedEvent());
+                    todoOptionDialog.cancel();
+                });
+                todoOptionDialog = new AlertDialog.Builder(context)
+                        .setView(dialogView)
+                        .create();
+                todoOptionDialog.show();
+                return true;
+            });
         }
     }
 
