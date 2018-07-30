bfa2dd06b3131f52e681e6c6e0f5da36f2936464
ZhangKe <zhangke_711@163.com>
2018-07-30 22:41:55 +0800

    修改启动页画面

--- app/src/main/java/com/zhangke/shizhong/page/main/LaunchActivity.java
+++ app/src/main/java/com/zhangke/shizhong/page/main/LaunchActivity.java
@@ -1,17 +1,32 @@
 package com.zhangke.shizhong.page.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import com.zhangke.shizhong.R;
 import com.zhangke.shizhong.page.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * 启动页面
- *
+ * <p>
  * Created by ZhangKe on 2018/7/29.
  */
 public class LaunchActivity extends BaseActivity {
+
+    @BindView(R.id.img_slogan_02)
+    ImageView imgSlogan02;
+    @BindView(R.id.img_slogan_01)
+    ImageView imgSlogan01;
 
     @Override
     protected int getLayoutResId() {
@@ -21,6 +36,23 @@ public class LaunchActivity extends BaseActivity {
 
     @Override
     protected void initView(@Nullable Bundle savedInstanceState) {
+        ButterKnife.bind(this);
 
+        final TranslateAnimation animation_01 = (TranslateAnimation) AnimationUtils.loadAnimation(LaunchActivity.this, R.anim.slogan_01);
+        final TranslateAnimation animation_02 = (TranslateAnimation) AnimationUtils.loadAnimation(LaunchActivity.this, R.anim.slogan_02);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(()->{
+            imgSlogan01.setVisibility(View.VISIBLE);
+            imgSlogan01.startAnimation(animation_01);
+            handler.postDelayed(()->{
+                imgSlogan02.setVisibility(View.VISIBLE);
+                imgSlogan02.startAnimation(animation_02);
+                handler.postDelayed(()->{
+                    startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                    finish();
+                }, 800);
+            }, 400);
+        }, 500);
     }
 }
