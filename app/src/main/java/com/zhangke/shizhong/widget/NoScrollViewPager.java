35cb3e3029a16d556ddcadb4625c8e4d7a0c260e
ZhangKe <zhangke_711@163.com>
2018-07-31 23:16:29 +0800

    修改统一按钮样式

---
+++ app/src/main/java/com/zhangke/shizhong/widget/NoScrollViewPager.java
@@ -0,0 +1,47 @@
+package com.zhangke.shizhong.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by ZhangKe on 2018/7/31.
+ */
+public class NoScrollViewPager extends ViewPager{
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        return false;
+    }
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        super.setCurrentItem(item, smoothScroll);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        super.setCurrentItem(item);
+    }
+
+
+}
